47b5a109aaa6fd8be52967ec22d839c8c51ac669
grad0ff@mail.ru <grad0ff130389>
2021-09-03 15:39:24 +0300

    Релиз BempIO v.1.2.3 Beta:   улучшения:   - нет   исправления:   - нет   разработка:   - изменение стиля  кнопок (в т.ч. нажатых) DI и DO при их срабатывании через методы класса

--- my_classes.py
+++ my_classes.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtGui import QFont
 
 
 class DI0_Button(QPushButton):
@@ -13,11 +14,39 @@ class DI0_Button(QPushButton):
             DI0_Button.PRESSED_BUTTONS -= 1
 
 
-class DI_Button(DI0_Button):
+class DIO_Button_Style(QPushButton):
+    def __init__(self, state='default'):
+        super(DIO_Button_Style, self).__init__()
+        self.state = state
+
+    def change_style(self):
+        if self.state == 'default':
+            self.setStyleSheet('background:  #f0f0f0')
+            self.setFont(QFont('MS Shell Dlg 2', 9, QFont.Normal))
+        else:
+            if self.state == 'checked':
+                self.setStyleSheet('background: rgb(50,255,50)')
+            elif self.state == 'pressed':
+                self.setStyleSheet('background: rgb(150,200,250)')
+            self.setFont(QFont('MS Shell Dlg 2', 10, QFont.Bold))
+
+
+class DI_Button(DI0_Button, DIO_Button_Style):
+    def __init__(self):
+        super(DI_Button, self).__init__()
+        self.state = 'default'
+
     def mousePressEvent(self, event):
         DI0_Button.mousePressEvent(self, event)
 
 
 class DO_Button(DI0_Button):
+    def __init__(self):
+        super(DO_Button, self).__init__()
+        self.state = 'unpressed'
+
     def mousePressEvent(self, event):
         DI0_Button.mousePressEvent(self, event)
+
+    def change_style(self):
+        if self.state == 'pressed':
